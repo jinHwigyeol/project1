2835b91ffa721f19e4473574b0016162717a1039
JinHwigyeol <100821809+jinHwigyeol@users.noreply.github.com>
2022-11-02 19:36:58 +0900

    Add files via upload

--- led_sever.py
+++ led_sever.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 import RPi.GPIO as GPIO
+from ast import Pass
+from bmp280 import BMP280
+from smbus2 import SMBus
 import time
 from gpiozero import LED
 import socket
 
+bus = SMBus(1) # use i2c bus1
+bmp280 = BMP280(i2c_dev=bus)
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
-p = GPIO.PWM(led, 500)
-p.start()
+p = GPIO.PWM(led_pin, 500)
+p.start(0)
 
 host = ''
 port = 10001
@@ -27,24 +32,33 @@ while True:
     #print("recevied command: {}".format(command.decode()))
     cmd = command.decode()
 
-
     try:
         if cmd.upper() == 'MID':
+            led.on()
             p.ChangeDutyCycle(50)
             reply = "LED turned MID"
             data_sock.sendall(reply.encode())
         elif cmd.upper() == 'OFF':
             led.off()
+            p.ChangeDutyCycle(0)
             reply = "LED turned OFF"
             data_sock.sendall(reply.encode())
         elif cmd.upper()== 'MAX':
+            led.on()
             p.ChangeDutyCycle(100)
             reply = "LED turned MAX"
             data_sock.sendall(reply.encode())
+        elif cmd.upper()== 'TEM':
+            temperature = bmp280.get_temperature()
+            pressure = bmp280.get_pressure()
+            print("Temperature = {:.2f}C".format(temperature))
+            print("Pressure = {:.1f}hPa".format(pressure))
+            print("=======================")
         else:
             reply = "command {} not supported".format(cmd)
             data_sock.sendall(reply.encode())
     except Exception as e:
         print("Exception: {}".format(str(e)))
     data_sock.close()
-    GPIO.cleanup()
+
+GPIO.cleanup()
